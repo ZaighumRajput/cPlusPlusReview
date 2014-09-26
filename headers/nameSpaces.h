4abce360d2c04f4b111fb7b9283eac0eccda039e
ZaighumRajput <zaighum47@gmail.com>
2014-09-26 22:01:59 +0200

    ex

--- headers/nameSpaces.h
+++
@@ -1,7 +0,0 @@
-#ifndef NAMESPACES_H
-#define	NAMESPACES_H
- 
-void blah();
-int mainFunction();
-
-#endif // !1
--- headers/pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef POINTERS_H
-#define POINTERS_H
-
-void bloo();
-void basicPoint();
-
-
-#endif
